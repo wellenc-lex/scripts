7270a0869c0c7251bde3df51269d64e36a5f111f
wellenc_lex <master>
2023-09-24 12:39:22 +0300

    leak impro

--- checkipVPN.sh
+++ checkipVPN.sh
@@ -1,17 +1,17 @@
 #* * * * * /root/tools/checkipVPN.sh >> /root/tools/tmp/outvpn.txt
 VPNip=`timeout 15 /opt/purevpn-cli/bin/purevpn-cli --info | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
 curl=`/usr/bin/curl -s -k --max-time 80 --connect-timeout 80 2ip.ru`
-echo $VPNip
-echo $curl
 
 if [ "$curl" != "$VPNip" ]; 
 then
+    echo $VPNip
+    echo $curl
     echo IP mismatch
     /usr/bin/docker pause $(/usr/bin/docker container ls -q) >/dev/null
-    timeout 15 /opt/purevpn-cli/bin/purevpn-cli -d && /usr/bin/pkill -f "openvpn"
-    connect=`timeout 40 /opt/purevpn-cli/bin/purevpn-cli -c FI`
+    timeout 30 yes|/usr/bin/sudo /opt/purevpn-cli/bin/purevpn-cli -d && /usr/bin/pkill -f "openvpn"
+    connect=`timeout 40 yes|/usr/bin/sudo /opt/purevpn-cli/bin/purevpn-cli -c FI`
     echo $connect
 else
-    echo OK
+    #echo OK
     /usr/bin/docker unpause $(/usr/bin/docker container ls -q) >/dev/null
 fi
--- leaksearch.sh
+++ leaksearch.sh
@@ -10,16 +10,18 @@ APIKEY=
 TMP="/root/tools/tmp/"
 for LINE in $LINES
 do
-	leakcheck --key "$APIKEY" "$LINE" --type domain_email -lo | sort -u >> "$TMP"newlines.txt
-	cat "$TMP"newlines.txt | anew "$TMP"allresults.txt > "$TMP"added-lines.txt
-	if [ -e "$TMP"added-lines.txt ]
-	then
-	  file_size=$(wc -c < "$TMP"added-lines.txt)
+        sleep 5
+        leakcheck --key "$APIKEY" "$LINE" --type domain_email -lo | sort -u >> "$TMP"newlines.txt
+        cat "$TMP"newlines.txt | anew "$TMP"allresults.txt > "$TMP"added-lines.txt
+        if [ -e "$TMP"added-lines.txt ]
+        then
+          file_size=$(wc -c < "$TMP"added-lines.txt)
 
-	  # Check if file is empty
-	  if [ "$file_size" -gt 1 ]
-	  then
-	    cat "$TMP"added-lines.txt | emissary --channel Discord -si -r 0
-	  fi
-	fi
+          # Check if file is empty
+          if [ "$file_size" -gt 1 ]
+          then
+            cat "$TMP"added-lines.txt >> "$TMP"added-linesBackup.txt
+            cat "$TMP"added-lines.txt | grep -i '.*:.*' | emissary --channel Discord -si -r 0
+          fi
+        fi
 done
