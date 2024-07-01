bb7816f285010d699a32afe0c106d7c488e31bde
wellenc_lex <master>
2024-07-01 17:19:12 +0300

    .

--- generateqr.sh
+++ generateqr.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 id=0
+summa="10 000 рублей, если ваш отчет подлежит оплате."
+count=99
 file="outputline.txt"
 
-# loop through all containers
-while [ $id -le 10 ]
+promo="
+Ваш промокод: "
+
+text="
+
+Найдите уязвимость и отправьте отчет в , указав промокод в описании. 
+
+Вы получите бонус к вознаграждению в размере"
+
+mkdir $count
+while [ $id -le $count ]
 do
   id=$((id+1))
-  line=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)
-  echo "Line: $line, ID=$id"
-  outfile=$(echo "$line" >> outfile.txt)
-  qrcode-svg -f -o $id.svg "$line" 
+  code=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32)
+  echo "Line: $code, ID=$id"
+  outfile=$(echo "$code" >> $count/outfile.txt)
+  qrcode-svg -f -o $count/$id.svg "$promo $code $text $summa" 
 done
